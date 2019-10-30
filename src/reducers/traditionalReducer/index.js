24942546910b12f1f933ab7b0eaebaa2699e93ce
Shivam Kantival <shivam.kantival@yahoo.co.in>
2019-10-30 15:05:51 +0530

    rename loading state reducer file

--- src/reducers/traditionalReducer/index.js
+++ src/reducers/traditionalReducer/index.js
@@ -4,11 +4,11 @@ import { combineReducers } from "redux";
 import normalisedArrayData from "./normalisedArrayData";
 import normalisedObjectData from "./normalisedObjectData";
 import primitiveDataType from "./primitiveDataType";
-import stateWithLoadingState from "./stateWithLoadingState";
+import stateWithLoading from "./stateWithLoading";
 
 export default combineReducers({
   normalisedArrayData,
   normalisedObjectData,
   primitiveDataType,
-  stateWithLoadingState,
+  stateWithLoading,
 });
